556be54192f0bd443d731571e57bd64bc502d3c7
Caroline Chisnall <caroline.chisnall@bbc.co.uk>
2020-12-03 08:09:07 +0000

    created stacks&amended script

--- updateStacks.sh
+++ updateStacks.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
-aws cloudformation deploy \
-  --template-file pipeline.yaml \
+aws cloudformation deploy --template-file pipeline.yaml --stack-name codepipeline-newapi \
   #--parameter-overrides $(cat ci/parameters.properties) \
-  --stack-name codepipeline-newapi \
   --role-arn arn:aws:iam::749144762306:role/Cloudformation-role12 \
   --capabilities CAPABILITY_IAM \
   --region eu-west-1
