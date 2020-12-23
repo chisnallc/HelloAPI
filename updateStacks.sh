1036a1a144f2b133e7afcf5e580363a5fd4ccefe
Manuel Pizani <manuel.pizani@bbc.co.uk>
2020-12-23 15:58:42 +0000

    Add new cloudformation role

--- updateStacks.sh
+++ updateStacks.sh
@@ -2,6 +2,6 @@
 set -e
 aws cloudformation deploy --template-file pipeline.yaml --stack-name codepipeline-newapi \
   #--parameter-overrides $(cat ci/parameters.properties) \
-  --role-arn arn:aws:iam::749144762306:role/Cloudformation-role12 \
+  --role-arn arn:aws:iam::749144762306:role/CloudformationRole-HelloAPI \
   --capabilities CAPABILITY_IAM \
   --region eu-west-1
