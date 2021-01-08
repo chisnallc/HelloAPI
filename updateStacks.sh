df45b9e1354906b7d2ff2faf26c74634859ee6c6
Manuel Pizani <manuel.pizani@bbc.co.uk>
2021-01-08 12:10:25 +0000

    Add parameter-overrides to updateStacks

--- updateStacks.sh
+++ updateStacks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 aws cloudformation deploy --template-file pipeline.yaml --stack-name codepipeline-newapi \
-  #--parameter-overrides $(cat ci/parameters.properties) \
+  --parameter-overrides $(cat parameters.properties) \
   --role-arn arn:aws:iam::749144762306:role/CloudformationRole-HelloAPI \
   --capabilities CAPABILITY_IAM \
   --region eu-west-1
