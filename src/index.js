eefe4bd232d79da92be6ed1ab22f680d559476c9
Manuel Pizani <manuel.pizani@bbc.co.uk>
2021-05-21 10:52:04 +0000

    Respond with 200 statuscode from lambda

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 exports.handler = async event => {
     try {
       console.log(`Event: ${event}`)
+      return {
+        statusCode: 200,
+        body: 'Hello'
+      }
     } catch (e) {
       console.log(e)
     }
