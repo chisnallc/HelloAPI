9d6d1dbedd41bd46c2c0d66da174464d14731256
Manuel Pizani <manuel.pizani@bbc.co.uk>
2021-08-13 14:03:39 +0000

    Add aws credentials on updateStacks.sh script

--- updateStacks.sh
+++ updateStacks.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+# Hit the following endpoint and fill the credentials below
+# https://aws-login.api.bbci.co.uk/account/749144762306/credentials
+
 set -e
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_SESSION_TOKEN=
+
+
 aws cloudformation deploy --template-file pipeline.yaml --stack-name codepipeline-newapi \
   --parameter-overrides $(cat parameters.properties) \
   --role-arn arn:aws:iam::749144762306:role/CloudformationRole-HelloAPI \
